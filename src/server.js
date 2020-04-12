8f30a02c928a4b1fb9f38d47ee6fc80872d9ff73
gabriel <gabrielmendes151@gmail.com>
2020-04-12 11:39:20 -0300

    Add crud to the project

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://...', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+app.listen(3333);
